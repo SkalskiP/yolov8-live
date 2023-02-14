9ddbd36a68f0ba572b67710e6259b1e6c47499ba
SkalskiP <piotr.skalski92@gmail.com>
2023-02-14 15:07:06 +0100

    YT video version

--- main.py
+++ main.py
@@ -7,71 +7,76 @@ import numpy as np
 
 
 ZONE_POLYGON = np.array([
-    [25, 25],
-    [1280 // 2 - 25, 25],
-    [1280 // 2 - 25, 720 - 25],
-    [25, 720 - 25]
+    [0, 0],
+    [0.5, 0],
+    [0.5, 1],
+    [0, 1]
 ])
 
 
-
-
-
 def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Pass secret configuration.')
-    parser.add_argument('--weights-path', type=str, required=True)
+    parser = argparse.ArgumentParser(description="YOLOv8 live")
+    parser.add_argument(
+        "--webcam-resolution", 
+        default=[1280, 720], 
+        nargs=2, 
+        type=int
+    )
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
 
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    model = YOLO(args.weights_path)
+    model = YOLO("yolov8l.pt")
 
     box_annotator = sv.BoxAnnotator(
-        thickness=2, 
-        text_thickness=2, 
-        text_scale=1)
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
 
-    zone = sv.PolygonZone(polygon=ZONE_POLYGON, frame_resolution_wh=(1280, 720))
-    zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.red())
+    zone_polygon = (ZONE_POLYGON * np.array(args.webcam_resolution)).astype(int)
+    zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=tuple(args.webcam_resolution))
+    zone_annotator = sv.PolygonZoneAnnotator(
+        zone=zone, 
+        color=sv.Color.red(),
+        thickness=2,
+        text_thickness=4,
+        text_scale=2
+    )
 
-    def process_frame(frame: np.ndarray) -> np.ndarray:
-        results = model(frame)[0]
-        detections = sv.Detections.from_yolov8(results)
+    while True:
+        ret, frame = cap.read()
+
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        detections = detections[detections.class_id == 47]
         labels = [
-            f"{model.model.names[class_id]} {confidence:0.2f}" 
-            for _, confidence, class_id, _ 
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
             in detections
         ]
         frame = box_annotator.annotate(
             scene=frame, 
-            detections=detections,
-            labels=labels)
-        # frame = sv.draw_polygon(
-        #     scene=frame, 
-        #     polygon=ZONE_POLYGON, 
-        #     color=sv.Color.red(), 
-        #     thickness=2)
-        zone.trigger(detections=detections)
-        frame = zone_annotator.annotate(scene=frame)
-        return frame
+            detections=detections, 
+            labels=labels
+        )
 
-    while True:
-        ret, frame = cap.read()
-        frame = process_frame(frame=frame)
-        cv2.imshow('webcam', frame)
+        zone.trigger(detections=detections)
+        frame = zone_annotator.annotate(scene=frame)      
+        
+        cv2.imshow("yolov8", frame)
 
         if (cv2.waitKey(30) == 27):
             break
 
-    cap.release()
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
